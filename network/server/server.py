97f60468b988f343ab701e2acfd51a50f839ea49
rikeda <rikeda@penguin>
2023-12-16 18:42:16 +0900

    [add] update

--- network/server/server.py
+++ network/server/server.py
@@ -1,28 +1,36 @@
 import socket
 import sys
+import os
+from take_photo import take_photo
 
 PORT = 4242
-
+REC_TIME = 10
 
 def main():
     if len(sys.argv) < 2:
         print("引数にラズパイのipアドレスを指定してください")
         exit(1)
     HOST = sys.argv[1]
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((HOST, PORT))
-    server_socket.listen()
-    print(f"サーバーが{HOST}:{PORT}で待機中...")
-    client_socket, client_address = server_socket.accept()
-    print(f"クライアントが{client_address}が接続しました")
-    file_path = './test.mp4'
-    with open(file_path, 'rb') as file:
-        data = file.read(1024)
-        while data:
-            client_socket.send(data)
+    try:
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.bind((HOST, PORT))
+        server_socket.listen()
+        print(f"サーバーが{HOST}:{PORT}で待機中...")
+        client_socket, client_address = server_socket.accept()
+        print(f"クライアントが{client_address}が接続しました")
+        file_path = take_photo('tmp.h264', REC_TIME)
+        with open(file_path, 'rb') as file:
             data = file.read(1024)
-    client_socket.close()
-    server_socket.close()
+            while data:
+                client_socket.send(data)
+                data = file.read(1024)
+        client_socket.close()
+        server_socket.close()
+        os.remove(file_path)
+    except socket.error as e:
+        print("socket error!!!!!!!!!!!!")
+        client_socket.close()
+        server_socket.close()
 
 if __name__ == "__main__":
     main()
--- network/server/take_photo.py
+++ network/server/take_photo.py
@@ -1,8 +1,15 @@
 from picamera2.encoders import H264Encoder
 from picamera2 import Picamera2
 import time
+import subprocess
+import os
 
-def recoding(encoder, file_path, shooting_time):
+def h264_to_mp4(input_file, output_file):
+    command = ["MP4Box", "-add", input_file, output_file]
+    subprocess.run(command)
+    os.remove(input_file)
+
+def recoding(camera, encoder, file_path, shooting_time):
     camera.start_recording(encoder, file_path)
     for i in range(shooting_time):
         print(f"{i + 1} / {shooting_time}")
@@ -14,8 +21,11 @@ def take_photo(file_path, shooting_time):
     camera = Picamera2()
     video_config = camera.create_video_configuration()
     camera.configure(video_config)
-    encoder = H264Encoder(bitrate=100000)
-    recoding(encoder, file_path, shooting_time)
+    encoder = H264Encoder(bitrate=10000000)
+    recoding(camera, encoder, file_path, shooting_time)
+    mp_file = file_path.split('.')[0] + '.mp4'
+    h264_to_mp4(file_path, mp_file)
+    return mp_file
 
 
 def main():
