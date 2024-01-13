44c2c0119bb647149cc64e419d606b627ef45eb1
rikeda <rikeda@penguin>
2024-01-13 15:23:18 +0900

    [update] プロセスを作成後にクラスのインスタンスを作成するように変更

--- sensor/camera/process_camera.py
+++ sensor/camera/process_camera.py
@@ -3,24 +3,26 @@ from multiprocessing import Array, Process
 from haar_like_camera import HaarLikeCamera
 
 
+def _color_ratio_loop(array, camera_class) -> None:
+    camera = camera_class()
+    while True:
+        color_ratio = camera.color_ratio()
+        for i, ratio in enumerate(color_ratio):
+            array[i] = ratio
+
+
 class ProcessCamera():
-    def __init__(self, camera=HaarLikeCamera()):
-        self.camera = camera
-        self.array = Array('i', self.camera.divisions)
-        self.process = Process(target=self.color_ratio_loop, args=())
+    def __init__(self, camera_class=HaarLikeCamera, divisions=40):
+        self.divisions: int = divisions
+        self.array = Array('i', self.divisions)
+        self.process = Process(target=_color_ratio_loop, args=(self.array, camera_class))
         self.process.start()
 
     def __del__(self):
         self.process.join()
 
-    def color_ratio_loop(self) -> None:
-        while True:
-            color_ratio = self.camera.color_ratio()
-            for i, ratio in enumerate(color_ratio):
-                self.array[i] = ratio
-
     def measure(self) -> List[int]:
-        return [self.array[i] for i in range(self.camera.divisions)]
+        return [self.array[i] for i in range(self.divisions)]
 
 
 if __name__ == '__main__':
--- sensor/ultrasonic/process_ultrasonic.py
+++ sensor/ultrasonic/process_ultrasonic.py
@@ -1,28 +1,29 @@
 from multiprocessing import Array, Process
-
 from ultrasonic import UltraSonic
 
 
+def _measure_loop(array, ultrasonic_class) -> None:
+    ultrasonic = ultrasonic_class()
+    while True:
+        data: dict[str, str] = ultrasonic.measure()
+        for i, value in enumerate(data.values()):
+            array[i] = int(value)
+
+
 class ProcessUltraSonic():
-    def __init__(self, ultrasonic=UltraSonic()):
-        self.ultrasonic = ultrasonic
-        self.array = Array('i', len(self.ultrasonic.ECHO_PIN))
-        self.process = Process(target=self.measure_loop, args=())
+    def __init__(self, ultrasonic=UltraSonic, number_of_sensor=10):
+        self.number_of_sensor = number_of_sensor
+        self.array = Array('i', self.number_of_sensor)
+        self.process = Process(target=_measure_loop, args=(self.array, ultrasonic))
         self.process.start()
 
     def __del__(self):
         self.process.join()
 
-    def measure_loop(self) -> None:
-        while True:
-            data: dict[str, str] = self.ultrasonic.measure()
-            for i, value in enumerate(data.values()):
-                self.array[i] = int(value)
-
     def measure(self) -> dict[str, str]:
         return {
             'd_' + str(i + 1): '{}'.format(self.array[i])
-            for i in range(len(self.ultrasonic.ECHO_PIN))
+            for i in range(self.number_of_sensor)
         }
 
 
