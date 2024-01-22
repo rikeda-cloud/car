bc2caeb5165a2423051e46d74fe6203d9e0aae92
Your Name <you@example.com>
2024-01-22 19:50:13 +0900

    [add] runシェルスクリプトrunを追加し、'run 1'のように実行することでプログラムを実行できるように変更,bashrcにperfomance/perfomanceディレクトリをPATHに追加しているので'./'をつけずに実行可能

--- perfomance/main.py
+++ perfomance/main.py
@@ -27,13 +27,28 @@ def run_minicar(car, camera, ultrasonic) -> None:
         print("Exit and Save")
 
 
-if __name__ == "__main__":
-    car = MiniCar(base_speed=346, model="only_camera_model_v2")
+def determine_ultrasonic(model: str):
+    if model == "only_camera_model_v2":
+        return ProcessUltraSonic(pool_size=2, timeout=0.035, echo_pin=[], trig_pin=[])
+    elif model == "c_d4_model_v1":
+        return ProcessUltraSonic(pool_size=2, timeout=0.035, echo_pin=[15, 21, 31, 33], trig_pin=[16, 22, 32, 35])
+    else:
+        return ProcessUltraSonic(pool_size=2, timeout=0.035)
+
+
+def main():
+    try:
+        model = sys.argv[1]
+        base_speed = int(sys.argv[2])
+    except:
+        model = "only_camera_model_v2"
+        base_speed = 346
+
+    car = MiniCar(base_speed=base_speed, model=model)
     camera = HaarLikeCamera(divisions=40, rect_height=20)
-
-    ## センサーを使用する数によって以下のコメントアウトを解除して使用してください
-    ultrasonic = ProcessUltraSonic(pool_size=2, timeout=0.035, echo_pin=[], trig_pin=[]) # センサーを使用しない
-    #ultrasonic = ProcessUltraSonic(pool_size=2, timeout=0.035, echo_pin=[15, 21, 31, 33], trig_pin=[16, 22, 32, 35]) # センサーを4つ使用するモデル
-    #ultrasonic = ProcessUltraSonic(pool_size=2, timeout=0.035) # センサーを10個使用するモデル
-
+    ultrasonic = determine_ultrasonic(model)
     run_minicar(car, camera, ultrasonic)
+
+
+if __name__ == "__main__":
+    main()
